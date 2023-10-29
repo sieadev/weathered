f96eeb46a402dc758343e48c95cea78f7024a036
SIEA <69807609+sieadev@users.noreply.github.com>
2023-10-30 00:51:12 +0100

    Add files via upload

---
+++ src/main/java/dev/siea/weathered/Weathered.java
@@ -0,0 +1,17 @@
+package dev.siea.weathered;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Weathered extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
